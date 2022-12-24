c521cfa0de38dd76b9fc75b9ead9da99d1eff20a
mlatysh199 <103229540+mlatysh199@users.noreply.github.com>
2022-12-24 01:01:11 -0600

    Day 9

---
+++ practice-1/031.608.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int msk(string j) {
+	int data = 0;
+	for (auto i : j) data |= 1 << (i - 'A');
+	return data;
+}
+
+char det(string j) {
+	if (j == "even") return 0;
+	if (j == "up") return 1;
+	return -1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	string data;
+	cin >> n;
+	while (n--) {
+		int msks[6];
+		bool r[3] = {0};
+		int nc = 0;
+		int pc = -1;
+		int appear = 0;
+		int rights = -1;
+		int lefts = -1;
+		for (int i = 0; i < 3; i++) {
+			cin >> data;
+			msks[i << 1] = msk(data);
+			appear |= msks[i << 1];
+			cin >> data;
+			msks[(i << 1) + 1] = msk(data);
+			appear |= msks[(i << 1) + 1];
+			cin >> data;
+			if (data == "up") {
+				r[i] = 1;
+				lefts &= msks[i << 1];
+				rights &= msks[(i << 1) + 1];
+			} else if (data == "down") {
+				r[i] = 1;
+				swap(msks[i << 1], msks[(i << 1) + 1]);
+				lefts &= msks[i << 1];
+				rights &= msks[(i << 1) + 1];
+			} else {
+				nc |= msks[i << 1];
+				nc |= msks[(i << 1) + 1];
+			}
+		}
+		pc &= ~nc;
+		pc &= appear;
+		pc &= 4095;
+		rights &= pc;
+		lefts &= pc;
+		// cout << 'p' << (bitset<32>)pc << '\n';
+		if (!(lefts & (lefts - 1)) && !rights) {
+			char pos = 0;
+			while (!(lefts&(1 << pos))) pos++;
+			pos += 'A';
+			cout << pos;
+			cout << " is the counterfeit coin and it is ";
+			cout << "heavy";
+			cout << ".\n";
+		} else if (!lefts && !(rights & (rights - 1))) {
+			char pos = 0;
+			while (!(rights&(1 << pos))) pos++;
+			pos += 'A';
+			cout << pos;
+			cout << " is the counterfeit coin and it is ";
+			cout << "light";
+			cout << ".\n";
+		}
+
+	}
+}
---
+++ practice-1/032.1061.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool TEST(char &msk, int pos) {
+	return (msk&(1 << pos)) > 0;
+}
+
+void SET(char &msk, int pos) {
+	msk |= (1 << pos);
+}
+
+char get_data(string in) {
+	in.pop_back();
+	char msk = 0;
+	if (in == "AB") {
+		SET(msk, 1);
+		SET(msk, 2);
+	} else if (in == "O") {
+		SET(msk, 0);
+	} else if (in == "A") {
+		SET(msk, 1);
+		SET(msk, 0);
+	} else {
+		SET(msk, 2);
+		SET(msk, 0);
+	}
+	return msk;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string a, b, c;
+	cin >> a >> b >> c;
+	int cnt = 1;
+	while (a != "E") {
+		vector<string> data;
+		if (c == "?") {
+			bool only_minus = a.back() == '-' && b.back() == '-';
+			char msk1 = get_data(a);
+			char msk2 = get_data(b);
+			if (TEST(msk1, 0) && TEST(msk2, 0)) data.push_back("O-");
+			if ((TEST(msk1, 1) && TEST(msk2, 2)) || (TEST(msk1, 2) && TEST(msk2, 1))) data.push_back("AB-");
+			if (TEST(msk1, 1) && (TEST(msk2, 1) || TEST(msk2, 0)) || TEST(msk2, 1) && (TEST(msk1, 1) || TEST(msk1, 0))) data.push_back("A-");
+			if (TEST(msk1, 2) && (TEST(msk2, 2) || TEST(msk2, 0)) || TEST(msk2, 2) && (TEST(msk1, 2) || TEST(msk1, 0))) data.push_back("B-");
+			char am = data.size();
+			if (!only_minus) for (int i = 0; i < am; i++) {
+				string temp = data[i];
+				temp.back() = '+';
+				data.push_back(temp);
+			}
+		} else {
+			string d = a, e = b;
+			if (d == "?") swap(e, d);
+			e = c;
+			bool has_minus = !(d.back() == '-' && c.back() == '+');
+			d.pop_back();
+			e.pop_back();
+			if (e == "O") {
+				if (d == "O") {
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				} else if (d == "A") {
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				} else if (d == "B") {
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				}
+			} else if (e == "A") {
+				if (d == "O") {
+					data.push_back("AB+");
+					data.push_back("A+");
+				} else if (d == "A") {
+					data.push_back("AB+");
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				} else if (d == "B") {
+					data.push_back("AB+");
+					data.push_back("A+");
+				} else {
+					data.push_back("AB+");
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				}
+			} else if (e == "B") {
+				if (d == "O") {
+					data.push_back("AB+");
+					data.push_back("B+");
+				} else if (d == "A") {
+					data.push_back("AB+");
+					data.push_back("B+");
+				} else if (d == "B") {
+					data.push_back("AB+");
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				} else {
+					data.push_back("AB+");
+					data.push_back("O+");
+					data.push_back("A+");
+					data.push_back("B+");
+				}
+			} else {
+				if (d == "AB") {
+					data.push_back("AB+");
+					data.push_back("A+");
+					data.push_back("B+");
+				} else if (d == "A") {
+					data.push_back("AB+");
+					data.push_back("B+");
+				} else if (d == "B") {
+					data.push_back("AB+");
+					data.push_back("A+");
+				}
+			}
+			char am = data.size();
+			if (has_minus) for (int i = 0; i < am; i++) {
+				string temp = data[i];
+				temp.back() = '-';
+				data.push_back(temp);
+			}
+			
+		}
+		cout << "Case " << cnt << ": ";
+		if (a == "?") {
+			if (data.empty()) cout << "IMPOSSIBLE";
+			else {
+				if (data.size() > 1) cout << '{';
+				for (int i = 0; i < data.size() - 1; i++) {
+					cout << data[i];
+					cout << ", ";
+				}
+				cout << data.back();
+				if (data.size() > 1) cout << '}';
+			}
+		} else cout << a;
+		cout << ' ';
+		if (b == "?") {
+			if (data.empty()) cout << "IMPOSSIBLE";
+			else {
+				if (data.size() > 1) cout << '{';
+				for (int i = 0; i < data.size() - 1; i++) {
+					cout << data[i];
+					cout << ", ";
+				}
+				cout << data.back();
+				if (data.size() > 1) cout << '}';
+			}
+		} else cout << b;
+		cout << ' ';
+		if (c == "?") {
+			if (data.size() > 1) cout << '{';
+			for (int i = 0; i < data.size() - 1; i++) {
+				cout << data[i];
+				cout << ", ";
+			}
+			cout << data.back();
+			if (data.size() > 1) cout << '}';
+		} else cout << c;
+		cout << '\n';
+		cnt++;
+		cin >> a >> b >> c;
+	}
+}
